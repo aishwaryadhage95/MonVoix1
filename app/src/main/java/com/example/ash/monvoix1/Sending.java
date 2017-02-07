af88fca571163dff1303979b87880f1cfc2772bf
Aishwarya Dhage <aishwarya@Aishwaryas-MacBook-Pro.local>
2017-02-07 22:20:26 +0530

    Initial commit

---
+++ app/src/main/java/com/example/ash/monvoix1/Sending.java
@@ -0,0 +1,13 @@
+package com.example.ash.monvoix1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Sending extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sending);
+    }
+}
